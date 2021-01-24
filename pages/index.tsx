414823a512a09c35fa3fca8f3a17f93351100780
pa-rang <appdler96@kaist.ac.kr>
2021-01-24 14:43:10 +0900

    [Style] Home: add home layout and style

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,45 @@ import { debounce } from "lodash";
 import search from "./search";
 import Image from "next/image";
 import Loading from "../components/common/Loading";
+import styled from "@emotion/styled";
+
+const Styled = {
+  MainWrapper: styled.main`
+    @media (max-width: 768px) {
+      margin-top: 126px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  `,
+  Header: styled.div`
+    @media (max-width: 768px) {
+      position: fixed;
+      top: 0;
+      padding: 12px 0;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+  `,
+  Logo: styled.img`
+    @media (max-width: 768px) {
+      height: 20px;
+    }
+  `,
+  MainCopy: styled.div`
+    @media (max-width: 768px) {
+      width: 220px;
+      margin-bottom: 12px;
+    }
+  `,
+  SubCopy: styled.div`
+    @media (max-width: 768px) {
+      width: 200px;
+      margin-bottom: 52px;
+    }
+  `,
+};
 
 type BookType = {
   title: string;
@@ -67,32 +106,26 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>리디밀리 - E북 찾을땐</title>
+        <title>이책저책 - e-book 찾을땐</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <h1>E-book을 찾아보세요!</h1>
+      <Styled.MainWrapper>
+        <Styled.Header>
+          <Styled.Logo src="/assets/images/logo.svg" />
+        </Styled.Header>
+        <Styled.MainCopy>
+          원하는 e-book이 어디에 있는지 이곳저곳 돌아다니지 마세요.
+        </Styled.MainCopy>
+        <Styled.SubCopy>
+          이책저책은 모든 e-book 구독 플랫폼과 판매 플랫폼의 검색결과를
+          제공합니다.
+        </Styled.SubCopy>
         <form onSubmit={getSearchBook} style={{ marginBottom: "32px" }}>
           <input
             style={{ width: "320px", height: "32px" }}
             value={inputValue}
             onChange={onChange}
           />
-          {/* <Link href="/search">
-            <a> */}
-          <button
-            type="submit"
-            style={{
-              height: "32px",
-              backgroundColor: "skyblue",
-              border: "none",
-            }}
-          >
-            검색하기
-          </button>
-          {/* </a>
-          </Link> */}
         </form>
         {isLoading ? (
           <Loading />
@@ -111,7 +144,7 @@ export default function Home() {
             );
           })
         )}
-      </main>
+      </Styled.MainWrapper>
     </div>
   );
 }

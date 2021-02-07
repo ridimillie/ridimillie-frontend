83a5d7fba68fe5eaa2a66b0e1434bc517f87ad93
pa-rang <appdler96@kaist.ac.kr>
2021-02-08 00:30:12 +0900

    [Fix] fast search result style

--- components/search/FastSearchResult.tsx
+++ components/search/FastSearchResult.tsx
@@ -18,7 +18,7 @@ const Styled = {
   Info: styled.div`
     display: flex;
     flex-direction: column;
-    padding-left: 16px;
+    padding: 12px;
   `,
   Title: styled.div`
     margin-bottom: 4px;

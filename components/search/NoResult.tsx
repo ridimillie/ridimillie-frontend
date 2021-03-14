8530f7c53eeb3194fd78dfb9f26f752cbfc27b03
pa-rang <appdler96@kaist.ac.kr>
2021-03-14 16:30:28 +0900

    [Style] fix no result ui

--- components/search/NoResult.tsx
+++ components/search/NoResult.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import style from '@emotion/styled';
+import styled from '@emotion/styled';
+
+const Styled = {
+  Root: styled.div`
+    letter-spacing: -0.02em;
+    padding-left: 12px;
+  `,
+};
 
 function NoResult() {
-  return <div style={{ letterSpacing: '-0.02em' }}>도서 검색 결과가 없습니다.</div>;
+  return <Styled.Root>도서 검색 결과가 없습니다.</Styled.Root>;
 }
 
 export default NoResult;

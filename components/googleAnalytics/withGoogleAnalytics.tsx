3c5bae4b60102ff6c2947e8a328c2c787cdac8d7
JunyupHong <junyup.hong@gmail.com>
2021-04-06 23:29:38 +0900

    [Dev] GA 컴포넌트 수정 및 gtag 모듈 분리

---
+++ components/googleAnalytics/withGoogleAnalytics.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import { gtag } from '../../lib/utils/GA';
+
+const withGoogleAnalytics = (children: React.ReactElement) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      window.dataLayer = window.dataLayer || [];
+      gtag('js', new Date());
+      gtag('config', 'G-ZWYN9S51KW', {
+        page_location: window.location.href,
+        page_path: window.location.pathname,
+        page_title: window.document.title,
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <script async src='https://www.googletagmanager.com/gtag/js?id=G-ZWYN9S51KW' />
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export default withGoogleAnalytics;
